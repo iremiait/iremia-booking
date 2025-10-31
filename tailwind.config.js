c75c6abb0b32c44004a109c37f1dcccf9aef5798
iremiait <iremiait@gmail.com>
2025-11-01 00:13:34 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        teal: {
+          50: '#f0fdfa',
+          100: '#ccfbf1',
+          200: '#99f6e4',
+          300: '#5eead4',
+          400: '#4FA5A8',
+          500: '#1B7B7E',
+          600: '#1B7B7E',
+          700: '#0D5456',
+          800: '#115e59',
+          900: '#134e4a',
+        }
+      },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+        serif: ['Georgia', 'serif'],
+      }
+    },
+  },
+  plugins: [],
+}
