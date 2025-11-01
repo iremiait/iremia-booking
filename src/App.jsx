ab458bdda99c545d9db4579fe137aef92927be3d
iremiait <iremiait@gmail.com>
2025-11-01 10:46:36 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="bg-white shadow-sm">
         <div className="max-w-7xl mx-auto px-4 py-6 sm:px-6 lg:px-8">
           <div className="flex items-center justify-center">
-            <img src="/logo.png" alt="Iremia" className="h-24" />
+            <img src="/logo.png" alt="Iremia" className="h-40" />
           </div>
         </div>
       </header>
