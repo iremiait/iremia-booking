9ec8cd47d92e0fe8df6d983a253210f79ba97ce2
iremiait <iremiait@gmail.com>
2026-05-30 11:54:06 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import AdminPopup from './pages/AdminPopup.jsx'
+import Privacy from './pages/Privacy.jsx'
+import Cookie from './pages/Cookie.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,6 +13,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/admin/popup" element={<AdminPopup />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/cookie" element={<Cookie />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
